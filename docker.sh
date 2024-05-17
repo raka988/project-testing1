6c29d20739b6585964e3853141023b025d5900d7
raka988 <raka988@gmail.com>
2024-05-17 20:56:32 +0700

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -22,8 +22,37 @@ docker run -it nama-image
 #create container with name
 docker run -it —name nama-container nama-image:versi
 
+#buat container menggunakan port dan nama
+docker run -dit -p 80:80 —name ct-nama-container nama-images
+
 #rename container 
 docker rename nama-lama nama-baru
 
+#menjalankan container
+docker exec -it nama-container /bin/bash
+
 #Mulai semua container yang dihentikan
 docker start $(docker ps -a -q)
+
+#Stop semua container yang running
+docker stop $(docker ps -q)
+
+#stop container
+docker stop nama-container 
+
+#hapus image docker
+docker rmi nama-iamge
+
+#hapus container
+docker rm nama-container
+docker rm nama-container --force
+
+#cek ip network
+docker network ls
+docker network inspect "NAME"
+
+#cara hubungkan container dengan network yang baru di buat
+docker network connect nama-network nama-container
+
+#install paket ping
+apt-get install -y iputils-ping
